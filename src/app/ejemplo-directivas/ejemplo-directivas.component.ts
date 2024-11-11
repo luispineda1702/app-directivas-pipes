ba3df47e7a1dbee04a3936576b4b34c03ab0e18f
luispineda1702 <ln72484067@gmail.com>
2024-11-11 00:17:59 -0500

    primer cambio

---
+++ src/app/ejemplo-directivas/ejemplo-directivas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo-directivas',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejemplo-directivas.component.html',
+  styleUrl: './ejemplo-directivas.component.css'
+})
+export class EjemploDirectivasComponent {
+
+}
