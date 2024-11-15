d569495ba475407a1a023ed886750c41d4938813
luispineda1702 <ln72484067@gmail.com>
2024-11-15 13:51:50 -0500

    ejercicio pipes teminado

--- src/app/ejemplo-directivas/ejemplo-directivas.component.ts
+++ src/app/ejemplo-directivas/ejemplo-directivas.component.ts
@@ -9,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class EjemploDirectivasComponent {
 
+  mostrarAlumnos = false
+  mostrarCursos = false
+
+  alumnosLista: Alumno=[]
+  cursosLista: Curso = []
+
+  verAlumnos():void{
+    this.mostrarAlumnos = true
+    this.alumnosLista.push({id:1,nombre:"Luis Angel",apellido:"Salvat"})
+    this.alumnosLista.push({id:2,nombre:"Juan Jose",apellido:"Mendoza"})
+    this.alumnosLista.push({id:3,nombre:"Maria Paz",apellido:"Calderon"})
+    this.alumnosLista.push({id:4,nombre:"Armando Paredes",apellido:"Rotas"})
+  }
+
+  verCursos():void{
+    this.mostrarCursos = true
+    this.cursosLista.push({id:1,nombre:"Desarrollo Web",precio:1230})
+    this.cursosLista.push({id:2,nombre:"Desarrollo Aplicaciones Móviles",precio:4030})
+    this.cursosLista.push({id:3,nombre:"Base de Datos 1",precio:2330})
+    this.cursosLista.push({id:4,nombre:"Scrum",precio:5230}) 
+  }
 }
+
+type Alumno = Array<{id:number; nombre:string; apellido:string}>
+type Curso = Array<{id:number; nombre:string; precio:number}>
--- src/app/ejemplo-pipes/ejemplo-pipes.component.ts
+++ src/app/ejemplo-pipes/ejemplo-pipes.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ejemplo-pipes',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './ejemplo-pipes.component.html',
   styleUrl: './ejemplo-pipes.component.css'
 })
 export class EjemploPipesComponent {
+  fechaActual = new Date()
 
+  nombres = "luis angel"
+
+  monto = 2321312
+
+  porcentaje = 0.2234
+
+  persona = {nombre:"Luis",apellido:"Salvat",rol:"Docente"}
 }
